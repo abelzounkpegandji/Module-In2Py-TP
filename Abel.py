422e263a8516837bfa2c19be0316fb30e00090ab
abelzounkpegandji <85234368+abelzounkpegandji@users.noreply.github.com>
2021-06-17 16:04:23 +0100

    Add files via upload

---
+++ Abel.py
@@ -0,0 +1,13 @@
+print('Exercice Dictionnaire')
+
+abel = {'python' : [12,14,18], 'html': [15,14,16], 'BDD':[15,12,16], 'service_reseau':[10,13,18], 'Proba': [16,14,16],
+        'stat' : [10,14,19],'SDA':[12,15,17]}
+        
+  print('Exercice E0 nombre premier')  
+  
+nombrepremier = [1,2,3,4,5,6,7,8,9,10]
+nombrepremier.append(22)
+nombrepremier.append(23,24)
+L1 =[]
+L2 =[]
+        
